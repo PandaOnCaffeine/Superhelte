484952c39574a93a9c3eeedb49c3db4d34566548
Tue Munch Madsen <tuepvm@gmail.com>
2022-08-29 12:23:23 +0200

    Add project files.

---
+++ Superhelte/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Superhelte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Superhero test = new Superhero();
+            test.ChangeToCostume();
+        }
+    }
+}
---
+++ Superhelte/Superhero.cs
@@ -0,0 +1,77 @@
+﻿namespace Superhelte
+{
+    public class Superhero
+    {
+        //instance variables
+        private string _secretIdentity;
+
+        private string _powers;
+
+        private string _maskAndCostume;
+
+        private string _moralCode;
+
+        private string _archEnemy;
+
+
+        //Get Set
+        //inkapsulating
+        public string SecretIdentity
+        {
+            get { return _secretIdentity; }
+            private set
+            {
+                _secretIdentity = value;
+            }
+        }
+
+        public string Powers
+        {
+            get { return _powers; }
+            private set
+            {
+                _powers = value;
+            }
+        }
+
+        public string MaskAndCostume
+        {
+            get { return _maskAndCostume; }
+            private set
+            {
+                _maskAndCostume = value;
+            }
+        }
+
+        public string MoralCode
+        {
+            get { return _moralCode; }
+            private set
+            {
+                _moralCode = value;
+            }
+        }
+
+        public string ArchEnemy
+        {
+            get { return _archEnemy; }
+            private set
+            {
+                _archEnemy = value;
+            }
+        }
+
+        //Methods
+        public void UsePowers()
+        {
+            Console.WriteLine("Uses Powers");
+        }
+
+        public void ChangeToCostume()
+        {
+            Console.WriteLine("Changes to costume");
+
+        }
+
+    }
+}
